541267cc1af6730297e85cba86adf4333f038122
rezaul islam <rezaul.cse.mbstu@gmail.com>
2022-03-30 12:01:56 +0600

    day one index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<head>
+    <!-- <title>PHP MySQL Batch:10</title> -->
+    <title><?php echo "PHP MySQL Batch:10" ?></title>
+</head>
+
+<body>
+    <?php echo "HI!" ?>
+    <h1>Web Developing using PHP & MySQL</h1>
+
+    <h2>From index.php</h2>
+    <?php echo "HELLO!" ?>
+</body>
