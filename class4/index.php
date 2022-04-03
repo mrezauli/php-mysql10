56bd568f55b47692b8c13eb5695818127a436a0e
rezaul islam <rezaul.cse.mbstu@gmail.com>
2022-04-03 12:19:30 +0600

    class 4

---
+++ class4/index.php
@@ -0,0 +1,144 @@
+<?php
+
+//says undefined variable
+//echo $basket;
+
+//variable part
+$basket;
+
+//$basket = 'apple';
+
+//$basket = 'orange';
+
+//$basket = 'orange, apple';
+
+//$basket = 'orange, apple, dates';
+
+//$basket = "orange, apple, dates";
+
+//$basket = "<h1>orange, apple, dates</h1>";
+
+//$basket = "<h1 style=" . "color:red" . ">orange, apple, dates</h1>";
+//<h1 style="color:red">orange, apple, dates</h1>
+
+//$basket = 123;
+//$basket = -123;
+//$basket = 0;
+
+//$basket = 100.456;
+//$basket = -100.456;
+//$basket = 0.000;
+//$basket = 0.5;
+//$basket = -0.5;
+
+//$basket = true;
+//$basket = false;
+
+//make an array
+//$basket = [101, 102, 103, 104, 105, 10.1, 20.2, 30.3 , 'orange', "apple"];
+// $basket = [
+//     10 => 101,
+//     20 => 102,
+//     30 => 103,
+//     'name1' => 104,
+//     'name2' => 105,
+//     60 => 10.1,
+//     70 => 20.2,
+//     80 => 30.3,
+//     'fruit1' =>'orange',
+//     'fruit2' => "apple"
+// ];
+
+//echos basket variable
+//echo $basket;
+
+//$basket as a class / multiple variale type
+
+// class Basket {
+//     public $name1 = 104;
+//     public $name2 = 105;
+//     public $fruit1 ='orange';
+//     public $fruit2 = "apple";
+// };
+
+//$basket = new Basket;
+
+$basket = null;
+
+//$BASKET = 'orange';
+//$BASKET = null;
+$BASKET;
+
+$bASKET = 'apple';
+
+$Basket = 1000;
+
+//echo output
+//echo $BASKET;
+
+//multiple echo
+//echo $basket, $bASKET;
+
+//multiple print
+//print $basket, $bASKET;
+
+
+//var_dump(echo $BASKET);
+//echo "<br  />";
+
+//print output
+//print $BASKET;
+//echo "<br  />";
+//var_dump(print $BASKET);
+
+//echo gettype($Basket);
+//var_dump(gettype($Basket));
+
+//echos $basket array
+//echo "<pre>";
+//var_dump($basket);
+//echo "</pre>";
+
+//echos $BASKET array
+//echo "<pre>";
+//var_dump($BASKET);
+//echo "</pre>";
+
+//echos $bASKET array
+//echo "<pre>";
+//var_dump($bASKET);
+//echo "</pre>";
+
+//echos $Basket array
+//echo "<pre>";
+//var_dump($Basket);
+//echo "</pre>";
+
+//if value undefined
+//$container;
+
+//echo "<pre>";
+//var_dump($container);
+//echo "</pre>";
+
+// means sigle line comment
+//$container;
+
+//echo "<pre>";
+//var_dump($container);
+//echo "</pre>";
+
+# means sigle line comment
+#$container;
+
+#echo "<pre>";
+#var_dump($container);
+#echo "</pre>";
+
+/* means sigle line comment
+$container;
+
+echo "<pre>";
+var_dump($container);
+echo "</pre>";
+*/
---
+++ class4/superglobals.php
@@ -0,0 +1,37 @@
+<?php
+
+// echo "<pre>";
+// var_dump($GLOBALS);
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($_SERVER);
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($_GET);
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($_POST);
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($_FILES);
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($_COOKIE);
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($_SESSION);
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($_REQUEST);
+// echo "</pre>";
+
+// echo "<pre>";
+// var_dump($_ENV);
+// echo "</pre>";
