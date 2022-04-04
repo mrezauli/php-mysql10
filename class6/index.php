fa2a531773702095b78b03affe5ebc3efa558abd
rezaul islam <rezaul.cse.mbstu@gmail.com>
2022-04-04 13:10:36 +0600

    class 6

---
+++ class6/index.php
@@ -0,0 +1,221 @@
+<?php
+
+// expression
+// $a = 5;
+// echo '$a=' . $a;
+// echo '<br />';
+
+// $b = $a;
+// echo '$b=' . $b;
+// echo '<br />';
+
+// function foo ()
+// {
+//     return 50;
+// }
+
+// $c = foo();
+// echo '$c=' . $c;
+// echo '<br />';
+
+//operator
+//$sum = 5 + 7;
+//echo $sum;
+// echo 5 + 7;
+// echo '<br />';
+
+// $sub = 5 - 7;
+// echo $sub;
+// echo 5 - 7;
+// echo '<br />';
+
+// $mul = 5 * 7;
+// echo $mul;
+// echo 'mul = ' . 5 * 7;
+// echo '<br />';
+
+// $div = 5 / 7;
+// echo $div;
+// echo 'div = ' . 5 / 7;
+// echo '<br />';
+
+// echo 'mod = ' . 5 % 7;
+// echo '<br />';
+
+// echo 'exp = ' . 5 ** 7;
+// echo '<br />';
+
+// $x = 100;
+// $y = 200;
+// echo $x;
+// echo '<br />';
+
+// echo $y;
+// echo '<br />';
+
+// echo $x += $y;
+// echo '<br />';
+// echo $x;
+// echo '<br />';
+// echo $y;
+// echo '<br />';
+
+// echo $x -= $y;
+// echo '<br />';
+// echo $x;
+// echo '<br />';
+// echo $y;
+// echo '<br />';
+
+// echo $x *= $y;
+// echo '<br />';
+// echo $x;
+// echo '<br />';
+// echo $y;
+// echo '<br />';
+
+// echo $x /= $y;
+// echo '<br />';
+// echo $x;
+// echo '<br />';
+// echo $y;
+// echo '<br />';
+
+// echo $x %= $y;
+// echo '<br />';
+// echo $x;
+// echo '<br />';
+// echo $y;
+// echo '<br />';
+
+// $x = 10;
+// $y = 20;
+// $z = 20;
+
+// var_dump($x == $y);
+// echo '<br />';
+
+// var_dump($z == $y);
+// echo '<br />';
+
+// var_dump($x === $y);
+// echo '<br />';
+
+// var_dump($z === $y);
+// echo '<br />';
+
+// var_dump($x != $y);
+// echo '<br />';
+
+// var_dump($z != $y);
+// echo '<br />';
+
+// var_dump($x <> $y);
+// echo '<br />';
+
+// var_dump($z <> $y);
+// echo '<br />';
+
+// var_dump($x !== $y);
+// echo '<br />';
+
+// var_dump($z !== $y);
+// echo '<br />';
+
+// var_dump($x > $y);
+// echo '<br />';
+
+// var_dump($z < $y);
+// echo '<br />';
+
+// var_dump($x >= $y);
+// echo '<br />';
+
+// var_dump($z <= $y);
+// echo '<br />';
+
+// var_dump($x <=> $y);
+// echo '<br />';
+
+// var_dump($y <=> $x);
+// echo '<br />';
+
+// var_dump($z <=> $y);
+// echo '<br />';
+
+// var_dump(++$x);
+// echo $x;
+// echo '<br />';
+
+// var_dump($x++);
+// echo $x;
+// echo '<br />';
+
+// var_dump(--$x);
+// echo $x;
+// echo '<br />';
+
+// var_dump($x--);
+// echo $x;
+// echo '<br />';
+
+//পার্সন ১ এবং পার্সন ২ রহিম (রহিমের নামের সমান) কিনা?
+//$personOne = 'Rahim'; // true
+// $personOne = 'Kamal'; // false
+// $personTwo = 'Karim'; // false
+//$personTwo = 'Rahim'; // true
+
+//var_dump(($personOne == "Rahim") && ($personTwo == "Rahim"));
+
+//পার্সন ১ অথবা পার্সন ২ রহিম (রহিমের নামের সমান) কিনা?
+//$personOne = 'Rahim'; // true
+//$personOne = 'Kamal'; // false
+//$personTwo = 'Karim'; // false
+//$personTwo = 'Rahim'; // true
+
+//var_dump(($personOne == "Rahim") || ($personTwo == "Rahim"));
+
+//পার্সন ১ সত্য কিনা?
+//$personOne = 'Rahim';
+//$personOne;
+
+//var_dump(!$personOne);
+
+$personOne = 'Rahim ';
+$personTwo = 'Karim';
+//echo $personOne, $personTwo;
+// print $personOne . $personTwo;
+// echo $personOne;
+// echo $personTwo;
+
+// echo $personOne;
+// echo "<br />";
+// $personOne .= $personTwo;
+// echo $personOne;
+// echo "<br />";
+// echo $personTwo;
+
+//$x = $personOne ? 1 : 2;
+// $x = $personThree ? 1 : 2;
+// echo $x;
+
+// $x = $personThree ?? 2;
+// $x = $personThree ?? $personOne;
+// $x = $personTwo ?? $personOne;
+// echo $x;
+
+//$x = $personThree ?? 2; $x = $personThree ?? $personOne; $x = $personTwo ?? $personOne; echo $x;
+// $x = $personThree ?? 2;
+
+
+
+// $x = $personThree ?? $personOne;
+
+
+
+
+
+
+// $x = $personTwo ?? $personOne;
+
+// echo $x;
